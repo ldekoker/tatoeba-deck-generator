5c961808fbc676a8c45d653f017adc0d148aa476
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-10 11:16:05 +1000

    Improve commenting
    Improved commenting for checkDirExists() and downloadDatabase()

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ DB_CONTENTS = set(['date.txt', 'links.csv', 'sentences_with_audio.csv',
                    'sentences_with_audio.uniq.csv', 'sentences.csv',
                    'sentences.escaped_quotes.csv', 'tags.csv', 'tags.escaped_quotes.csv'])
 
-def checkDirExists(path):
+def checkPathExists(path):
+    """
+    Returns True if the given path exists.
+    """
     return os.path.exists(path)
 
 def checkDatabase():
@@ -16,7 +19,7 @@ def checkDatabase():
     Checks if the user already has the tatoeba database, and that if they do it isn't too old.
     """
     # Check if database folder exists
-    if checkDirExists(DIR_PATH):
+    if checkPathExists(DIR_PATH):
         contents = os.listdir(DIR_PATH)
 
         # Check if it has the database files
@@ -36,16 +39,19 @@ def checkDatabase():
     else:
         return False
 
-def downloadDatabase():
+def downloadTatoebaData():
     """
     Downloads the database.
     """
-    if checkDirExists(DIR_PATH):
+    # If the path to the database already exists, then it must be incomplete or
+    # more than 6 months old, so it must be deleted and replaced.
+    if checkPathExists(DIR_PATH):
         rmtree(DIR_PATH)
     
+    # Run the shell commands to download all of the files.
     subprocess.run(['wsl', 'bash', 'download_and_prepare.sh'], check=True)
 
-    # create date file
+    # Create a date file to mark the age of the downloaded database.
     today = datetime.date.today()
     year, month, day = today.year, today.month, today.day
     date_text = f"{year} {month} {day}"
@@ -53,11 +59,12 @@ def downloadDatabase():
     date_file.write(date_text)
     date_file.close()
 
+    print("Data downloaded from the internet. Next, data will be imported into a database.")
 
 def main():
     # Download database if user doesn't already have it, or their copy is too old
     if not checkDatabase():
-        downloadDatabase()
+        downloadTatoebaData()
     
     # Make query
     # Download audio files
