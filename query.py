ea319f81e123435b739b397b6ac5c10bfd8d3c91
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-11 13:12:16 +1000

    Improve commenting and structure of query.py

--- query.py
+++ query.py
@@ -2,70 +2,73 @@ import csv
 import os
 import sqlite3
 
-output_dir = 'output'
-native_langs = []
-
-
 def native_lang_columns():
-        def native_lang_column(lang):
-                return f"""
-                "<ul class=""translations""><li>" ||
-                (
-                                SELECT group_concat(sentences.text, "</li><li>")
-                                FROM links JOIN sentences
-                                ON
-                                                links.translation_id = sentences.sentence_id
-                                WHERE
-                                                links.sentence_id = target_sentences.sentence_id
-                                                AND
-                                                sentences.lang = '{lang}'
-                                                )
-                || "</li></ul>"
-                """
-        result = ""
-        for lang in native_langs[:-1]:
-                result += native_lang_column(lang) + ", "
-        result += native_lang_column(native_langs[-1])
-        return result
-
+    """
+    Create an SQL query for each of the given native languages. Will be inserted into the makeCardsCSV query.
+    """
+    def native_lang_column(lang):
+        return f"""
+        "<ul class=""translations""><li>" ||
+        (
+            SELECT group_concat(sentences.text, "</li><li>")
+            FROM links JOIN sentences
+            ON
+                links.translation_id = sentences.sentence_id
+            WHERE
+                links.sentence_id = target_sentences.sentence_id
+                AND
+                sentences.lang = '{lang}'
+                )
+        || "</li></ul>"
+        """
+    result = ""
+    for lang in native_langs[:-1]:
+        result += native_lang_column(lang) + ", "
+    result += native_lang_column(native_langs[-1])
+    return result
 
 def makeCardsCSV(target, natives, database="./database/tatoeba.sqlite3"):    
-        global native_langs
-        native_langs = natives.split(" ")
+    """
+    Queries the SQLite database to generate a csv composed of target language sentences, with audio, that have native language translations.
+    """
+    native_langs = natives.split(" ")
 
-        conn = sqlite3.connect(database)
-        c = conn.cursor()
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
 
-        query = f"""
-        SELECT
-                        target_sentences.sentence_id,
-                        target_sentences.text,
-                        "[sound:tatoeba_" || "{target}" || "_" || target_sentences.sentence_id || ".mp3]",
-                        "<ul class=""tags""><li>" ||
-                        (
-                                SELECT group_concat(tag_name, "</li><li>")
-                                FROM tags
-                                WHERE tags.sentence_id = target_sentences.sentence_id
-                        )
-                        || "</li></ul>",
-                        {native_lang_columns()}
-        FROM
-                        sentences AS target_sentences
-        WHERE
-                        target_sentences.lang = "{target}" AND
-                        target_sentences.sentence_id IN (SELECT sentence_id FROM sentences_with_audio)
-        ;
-        """
-        if not os.path.exists('output'):
-                os.makedirs('output')
-        with open(f'{os.path.join(output_dir, target)} → {natives}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-                out = csv.writer(csvfile, delimiter='\t',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                for row in c.execute(query):
-                        out.writerow(row)
+    # Create the SQL Query
+    query = f"""
+    SELECT
+        target_sentences.sentence_id,
+        target_sentences.text,
+        "[sound:tatoeba_" || "{target}" || "_" || target_sentences.sentence_id || ".mp3]",
+        "<ul class=""tags""><li>" ||
+        (
+            SELECT group_concat(tag_name, "</li><li>")
+            FROM tags
+            WHERE tags.sentence_id = target_sentences.sentence_id
+        )
+        || "</li></ul>",
+        {native_lang_columns(native_langs)}
+    FROM
+        sentences AS target_sentences
+    WHERE
+        target_sentences.lang = "{target}" AND
+        target_sentences.sentence_id IN (SELECT sentence_id FROM sentences_with_audio)
+    ;
+    """
+    # Make an output directory
+    if not os.path.exists('output'):
+        os.makedirs('output')
 
-        conn.close()
+    # Create the CSV in the output directory
+    with open(f'{os.path.join('output', target)} → {natives}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        out = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
+        for row in c.execute(query):
+            out.writerow(row)
+
+    conn.close()
 
 if __name__ == '__main__':
         makeCardsCSV('fin', 'eng rus ita')
